1ee7aed09ffc494142731d90779ae92b60933717
eddydinh <minhdinh25599@gmail.com>
2019-11-26 02:41:12 -0800

    update projects

--- js/main.js
+++ js/main.js
@@ -34,9 +34,25 @@ function closeModal() {
         $('.modal-content-container').removeClass('animateScale');
     }, 500);
     document.getElementById('myModal').style.display = "none";
+    
+    resetDisplay();
 
 }
 
+function resetDisplay(){
+    $("#firstPic").show();
+    $("#iframeVid").hide();
+    $("#iframeVid").attr('src',"");
+    $("#prevArrow").show();
+    $("#forwardArrow").show();
+}
+
+function displayFirstIframe(){
+    $("#firstPic").hide();
+    $("#iframeVid").show();
+    $("#prevArrow").hide();
+    $("#forwardArrow").hide();
+}
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -87,46 +103,69 @@ const turnBackRed = (e) => {
     });
 };
 const modalInfoOf = (projectname) => {
-    
+    var title, subtext, descrip, link;
     if (projectname === "wandrscape") {
-        const title = "WANDRSCAPE";
-        const subtext = "The Centre for Culture and Technology";
-        const descrip = "Wandrscape takes in information about all the artworks scattered around UBC-Okanagan Campus and display them as markers on a real-time rendered map. A responsive web-app written in Jquery, PHP intergrated with Google Maps API.";
-        const link = "https://digitalmedia.ok.ubc.ca/wandrscape/";
-        PassInfoToModal(projectname, title, subtext, descrip, link);
+        title = "WANDRSCAPE";
+        subtext = "The Centre for Culture and Technology";
+        descrip = "Wandrscape takes in information about all the artworks scattered around UBC-Okanagan Campus and display them as markers on a real-time rendered map. A responsive web-app written in Jquery, PHP intergrated with Google Maps API.";
+        link = "https://digitalmedia.ok.ubc.ca/wandrscape/";
+        
     } else if (projectname === "naramata") {
-        const title = "NARAMATA PHOTO VIEWER APP";
-        const subtext = "Kelowna Naramata Museum";
-        const descrip = "This simple web-app accepts information of close to a thousand photos to efficiently load, sort, and display them on a touch screen. A combination of CSS, Javascript, and HTML Canvas provides the users with intuitive and dynamic experiences.";
-        const link = "https://digitalmedia.ok.ubc.ca/naramata/";
-        PassInfoToModal(projectname, title, subtext, descrip, link);
+        title = "NARAMATA PHOTO VIEWER APP";
+        subtext = "Kelowna Naramata Museum";
+        descrip = "This simple web-app accepts information of close to a thousand photos to efficiently load, sort, and display them on a touch screen. A combination of CSS, Javascript, and HTML Canvas provides the users with intuitive and dynamic experiences.";
+        link = "https://digitalmedia.ok.ubc.ca/naramata/";
+        
     }else if (projectname === "soundscape") {
-        const title = "SOUNDSCAPE";
-        const subtext = "Kettle Valley Railway";
-        const descrip = "Scalable, resilient backend built from scratch with Node.js, PostgreSQL. Responsive frontend built with REACT in combination with Redux. The web-app provides a “sound-walk” experience. That is, the audio outputs respond to user’s current location.";
-        const link = "https://digitalmedia.ok.ubc.ca/kvr/admin";
-        PassInfoToModal(projectname, title, subtext, descrip, link);
+        title = "SOUNDSCAPE";
+        subtext = "Kettle Valley Railway";
+        descrip = "Scalable, resilient backend built from scratch with Node.js, PostgreSQL. Responsive frontend built with REACT in combination with Redux. The web-app provides a “sound-walk” experience. That is, the audio outputs respond to user’s current location.";
+        link = "https://digitalmedia.ok.ubc.ca/kvr/admin";
+      
     }
     
     else if (projectname === "ourloveboard") {
         
-        const title = "OUR LOVE BOARD";
-        const subtext = "A Chrome Extension";
-        const descrip = "A Chrome Extension built from React.js, Redux, Chrome API, and Firebase Users can sign up for an account and pair up with a partner. The couple will share a same digital drawing board, which they can use to leave each other daily romantic messages.";
-        const link = "https://chrome.google.com/webstore/detail/ourloveboard/onlndhcgomlelaijooogpbhdelhmhepj";
-        PassInfoToModal(projectname, title, subtext, descrip, link);
+        title = "OUR LOVE BOARD";
+        subtext = "A Chrome Extension";
+        descrip = "A Chrome Extension built from React.js, Redux, Chrome API, and Firebase Users can sign up for an account and pair up with a partner. The couple will share a same digital drawing board, which they can use to leave each other daily romantic messages.";
+        link = "https://chrome.google.com/webstore/detail/ourloveboard/onlndhcgomlelaijooogpbhdelhmhepj";
+       
     }
+    else if (projectname === "theGame"){
+        title = "WELCOME TO THE GAME";
+        subtext = "A Java remade";
+        descrip = " A Java remake of a popular game, adhering strictly to OOP principles and practices with design patterns and data structures for optimal coherence, efficiency, and minimal coupling. Accompanied by thorough unit tests.";
+        link = "https://github.com/eddydinh/WelcomeToTheGameJava";
+        
+    }
+    
+    PassInfoToModal(projectname, title, subtext, descrip, link);
 
 };
 const PassInfoToModal = (projectname, title, subtext, description, link) => {
     $(".mySlides").each(function (index) {
+        $($(this).children()[0]).css('width', "100%");
         let imageSource = '';
-        if ((projectname=="ourloveboard") && (index == 1)) {
+        if((projectname=="theGame") && (index == 0)){
+            displayFirstIframe();
+            $("#iframeVid").attr('src', "https://www.youtube.com/embed/HFvxQrYmhng?cc_load_policy=1&cc_lang_pref=en");
+            
+        }
+        else if ((projectname=="ourloveboard")&& (index == 0)){
+           
+       
             imageSource = "images/" + projectname + index + ".gif";
+            
         }else{
             imageSource = "images/" + projectname + index + ".png"; 
         }
         
+        
+        if(projectname=="ourloveboard"){
+             $($(this).children()[0]).css('width', "45%");
+        }
+    
 
         
 
